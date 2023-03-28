3d71a8f3bdfb446049b4a997e3af4078bb0e10d7
Facundo Javier Gelatti <javiergelatti@gmail.com>
2023-03-27 22:07:13 -0300

    :art: simplify and reorganize code

--- runtime-compile.rb
+++ runtime-compile.rb
@@ -1,52 +1,39 @@
 class Console
     def initialize(output_location)
-        @output_location = output_location
-        @output_file = File.open(@output_location, "w")
+        @output_file = File.open(output_location, "w")
+    end
+
+    def write(&message)
+        @output_file.puts(message.call)
+    rescue Exception
+        @output_file.puts(error_message)
+    ensure
+        @output_file.flush
+    end
+
+    private
+
+    def error_message
+        "Oops, hubo un error. #{random_utf_emoji}"
     end
 
     def random_utf_emoji
         [0x1F600 + rand(80)].pack("U*")
     end
-    
-    def error
-        "Oops, hubo un error. " + random_utf_emoji()
-    end
-
-    def write(&message)
-        begin
-        @output_file.write(message.call)
-        rescue Exception => e
-            @output_file.write(error())
-        ensure    
-            @output_file.flush
-        end
-    end
-
-    def new_line
-        @output_file.write("\n")
-    end
-
 end
-
-@console = Console.new("console.txt")
 
 class Calculadora
     def dividir(a, b)
-            a / b
+        a / b
     end 
-
 end
 
-def run
-    Thread.new do
-        loop do
-            sleep(1)
-            @console.write do
-                Calculadora.new.dividir(3,0)
-            end
-            @console.new_line()
+console = Console.new("console.txt")
+Thread.new do
+    loop do
+        sleep(1)
+        console.write do
+            Calculadora.new.dividir(3, 0)
         end
     end
 end
-
-run
